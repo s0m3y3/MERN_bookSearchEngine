b9d2b7496b9ed35a9d6ad85a92b768866cdb9b68
Yee Y <s0m3y3@gmail.com>
2023-11-11 07:29:40 -0600

    got server up and running.

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://yaX102:qGyiTIORHrOTuADD@yax102.tkf597m.mongodb.net/');
 
 module.exports = mongoose.connection;
